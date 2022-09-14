c045ef89ec636d0fad1addd438c891e3570c9848
jiayuqi <541443699@qq.com>
2022-09-14 11:04:44 +0800

    8.2项目监控

--- src/main/java/com/nowcoder/community/config/SecurityConfig.java
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                 )
                 .antMatchers(
                         "/discuss/delete",
-                        "/data/**"
+                        "/data/**",
+                        "/actuator/**"
                 )
                 .hasAnyAuthority(
                         AUTHORITY_ADMIN
